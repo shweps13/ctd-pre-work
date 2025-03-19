5abe3bad5c1746cf651c74cc3b3b91aa1bda51c0
Joe Siburov <g.siburov@gmail.com>
2025-03-18 22:37:42 -0700

    feat(package.json): landed prettier + tailwind
    added styling

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react", "@typescript-eslint"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+};
